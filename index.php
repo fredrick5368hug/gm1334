a76626be8088b4d766f825e8d469d2b819ab9dce
fredrick5368hug <57219566+fredrick5368hug@users.noreply.github.com>
2020-07-26 21:45:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.fortunas.biz/about-us/");
+header("Location: https://www.amazon.in/s?k=asharfi+fabrics&crid=1GS28G7F05Y6V&sprefix=asharfi+fabr%2Caps%2C355&ref=nb_sb_ss_i_1_12");
 die();
 ?>
