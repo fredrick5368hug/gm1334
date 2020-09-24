890f2a8cee60c4584593e5080f0e4b0e0b957bf4
fredrick5368hug <57219566+fredrick5368hug@users.noreply.github.com>
2020-09-24 15:43:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.amazon.in/s?k=asharfi+fabrics&crid=3OIAF4XM0W1WF&sprefix=asharfi+fab%2Caps%2C438&ref=nb_sb_ss_i_1_11");
+header("Location: https://uk.norton.com/nortonservices?inid=nortoncom_nav_nortonservices_homepage:home");
 die();
 ?>
